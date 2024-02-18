63c5c2fd74a532982851969f599ccc42c9a21b6e
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-02-18 21:03:00 +0800

    Modify task_params to include task_description

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -64,6 +64,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:name, :completed)
+      params.require(:task).permit(:name, :completed, :task_description)
     end
 end
